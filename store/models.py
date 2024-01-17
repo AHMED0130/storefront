b99131222de70183c6422fbbcd7529c65d19f44e
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-01-17 15:54:08 +0200

    fixed error in the productimage model

--- store/models.py
+++ store/models.py
@@ -49,7 +49,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store\images',
+    image = models.ImageField(upload_to='store/images',
                               validators=[validate_file_size,])
 
 
