6a3230670ed8e7474f1e6889909306fe1b4c7f16
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-02-03 17:03:17 +0200

    configured the sqlite3 instead of mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -97,14 +97,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 
 # Password validation
