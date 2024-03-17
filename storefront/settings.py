750ae2feb9de876a60eb469170d0438f587e963e
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-03-17 16:49:39 +0200

    configure mysql as a DBMS

--- storefront/settings.py
+++ storefront/settings.py
@@ -93,15 +93,19 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#sqlite for testing purposes only
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'store'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
+
+
+
 
 
 
