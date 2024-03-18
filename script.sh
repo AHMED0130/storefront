4979259ccb75e1618342a23a7357ef4c2257a680
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-03-18 09:12:15 +0200

    remove environment section form deploy stage

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 export IMAGE=$1
-export USERNAME=$2
-export PASS=$3
-echo $PASS | docker login -u $USERNAME --password-stdin
 echo "IMAGE variable: ${IMAGE}"
 docker-compose -f docker-compose.yml up --detach
